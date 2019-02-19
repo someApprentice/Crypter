779d929ee83f0d6f9aa135fba2a48b7410bd37c8
someApprentice <someApprentice@protonmail.com>
2019-02-19 16:53:21 +0700

    Added schema.sql

---
+++ schema.sql
@@ -0,0 +1,61 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 11.2 (Debian 11.2-1.pgdg90+1)
+-- Dumped by pg_dump version 11.2 (Debian 11.2-1.pgdg90+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: uuid-ossp; Type: EXTENSION; Schema: -; Owner: 
+--
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp" WITH SCHEMA public;
+
+
+--
+-- Name: EXTENSION "uuid-ossp"; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION "uuid-ossp" IS 'generate universally unique identifiers (UUIDs)';
+
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- Name: user; Type: TABLE; Schema: public; Owner: crypter
+--
+
+CREATE TABLE public."user" (
+    uuid uuid DEFAULT public.uuid_generate_v4() NOT NULL,
+    email character varying(255) NOT NULL,
+    name character varying(255) NOT NULL,
+    hash character varying(60) NOT NULL
+);
+
+
+ALTER TABLE public."user" OWNER TO crypter;
+
+--
+-- Name: user user_pkey; Type: CONSTRAINT; Schema: public; Owner: crypter
+--
+
+ALTER TABLE ONLY public."user"
+    ADD CONSTRAINT user_pkey PRIMARY KEY (uuid);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
